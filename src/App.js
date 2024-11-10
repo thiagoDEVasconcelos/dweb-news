71266779e461511907ead854ede05bde501e644b
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2024-11-09 22:59:53 -0300

    Iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          iniciando em 3 2 1...
         </p>
         <a
           className="App-link"
